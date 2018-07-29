7057bfd0d05679f92968a4c47e48ddd91301ec0a
Guilherme Damascena <guiga_cfc@hotmail.com>
2018-07-29 20:58:48 -0300

    Update - class 3

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,32 +1,28 @@
-class NegociacaoController{
+class NegociacaoController {
 
-    constructor(){
+    constructor() {
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
 
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
-
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
+        this._listaNegociacoes = new Bind(new ListaNegociacoes(), new NegociacoesView($('#negociacoesView')), 'adiciona', 'esvazia');
+        this._mensagem = new Bind(new Mensagem(), new MensagemView($('#mensagemView')), 'texto');
     }
 
-    adiciona(event){
+    adiciona(event) {
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criarNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
-
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
-
         this._limpaFormulario();
     }
 
-    _criarNegociacao(){
+    apaga() {
+        this._listaNegociacoes.esvazia();
+        this._mensagem = 'Negociações apagadas com sucesso';
+    }
+
+    _criarNegociacao() {
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
@@ -34,7 +30,7 @@ class NegociacaoController{
         );
     }
 
-    _limpaFormulario(){
+    _limpaFormulario() {
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
